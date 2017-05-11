4af86ac95b0de9673546af01812c21a538d71051
Morita Yuto <melbre0612@gmail.com>
2017-05-11 10:19:06 +0900

    表裏マン

--- docs/player.js
+++ docs/player.js
@@ -1,3 +1,43 @@
-function action(oppornent){
-  return 1;
+const GU = 0;
+const CHOKI = 1;
+const PA = 2;
+
+
+let matches = 0;
+function action(opponent){  
+  matches = matches + 1;
+  let amari = matches % 2;
+  let isEven = amari == 0;
+  console.log(`${matches} % 2 = ${amari} : ${isEven}`);
+  if(isEven){
+    return GU;
+  }
+return PA;
 }
+
+
+/* 表裏マン
+let matches = 0;
+  function action(opponent){
+   matches = matches + 1;
+   if(matches % 2 == 1){
+return PA;
+   }else{
+return GU;
+   }
+ }
+*/
+ /*　順番に出すマン
+ let matches = 0;
+  function action(opponent){
+   matches = matches + 1;
+   if(matches % 3 == 1){
+return PA;
+  }if else(matches % 3 == 2){
+return GU;
+  }
+  else{
+    return CHOKI;
+  }
+ }
+ */
