62e528e9a4d2abb2961e80f770d63b6954b73edb
Morita Yuto <melbre0612@gmail.com>
2017-05-18 11:48:59 +0900

    順番に勝つマンまで全部勝てる

--- docs/player.js
+++ docs/player.js
@@ -1,11 +1,15 @@
 const GU = 0;
 const CHOKI = 1;
 const PA = 2;
-
-
 let matches = 0;
+　
+function CountUp(){
+   matches = matches + 1;
+}
+
+//帰ってきた表裏マン　on-third
 function actionAgainstOmoteUra2(){
-  matches = matches + 1;
+  CountUp();
   const isEven = matches % 3 == 0;
   let result = GU;
   if(isEven){
@@ -14,41 +18,58 @@ function actionAgainstOmoteUra2(){
     return result;
 }
 
-function action(opponent){  
-  let result = actionAgainstOmoteUra2();
+//表裏マン　odd-even
+  function actionOmoteUra(){
+    CountUp();
+    const isEven = matches % 2 == 0;
+    let result = PA;
+    if(isEven){
+      return result = GU;
+    }
+    return result;
+  }
+  
+  
 
+//チョキ大好きマン　choki-lover
+function CHOKIdaisukiman(){
+  let result = GU;
   return result;
+
 }
 
-
-
-
-
-/* 表裏マン
-let matches = 0;
-  function action(opponent){
-   matches = matches + 1;
-   if(matches % 2 == 1){
-return PA;
-   }else{
-return GU;
-   }
- }
-*/
-
-
-
- /*　順番に出すマン
- let matches = 0;
-  function action(opponent){
-   matches = matches + 1;
-   if(matches % 3 == 1){
-return PA;
-  }if else(matches % 3 == 2){
-return GU;
+ //　順番に出すマン　rotation
+  function actionOrder(){
+   CountUp();
+   const isEven = matches % 3;
+   let result = CHOKI;
+   if(isEven == 1){
+return result = PA;
+  }else if(isEven == 2){
+return result = GU;
   }
   else{
-    return CHOKI;
+    return result;
   }
  }
- */
+
+ function action(opponent){
+console.log(opponent);
+
+if(opponent =='fighter::choki-lover'){
+result = CHOKIdaisukiman();
+}
+else if(opponent =='fighter::odd-even'){
+  
+  result = actionOmoteUra();
+}
+else if(opponent =='fighter::on-third'){
+  
+  result = actionAgainstOmoteUra2();
+}
+else{
+  result = actionOrder();
+}
+return result;
+
+ }
